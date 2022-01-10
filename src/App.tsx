6d1d31f15d691c92f8beb25735fc1830ea5d3349
valeria-ab <lera.magnum@gmail.com>
2022-01-10 21:47:27 +0300

    updated gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import {HashRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import './App.css';
-import Main from './UI/Main';
-import store from './BLL/store/store';
-
-
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./App.css";
+import Main from "./UI/Main";
+import store from "./BLL/store/store";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <HashRouter>
-                <Provider store={store}>
-                    <Main/>
-                </Provider>
-            </HashRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <HashRouter>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </HashRouter>
+    </div>
+  );
 };
 
 export default App;
