a76f63df7bd30c0ee94560712d2984f78faf1ee9
Dmity-kl <i6169696@gmail.com>
2022-01-13 15:52:43 +0300

    registration

--- src/BLL/register/registerReducer.ts
+++ src/BLL/register/registerReducer.ts
@@ -1,20 +1,36 @@
-import { registerInitialState } from "./registerInitialState";
-import { IRegisterActions, REGISTER } from "./registerActions";
 
-export const registerReducer = (
-  state = registerInitialState,
-  action: IRegisterActions
-) => {
-  switch (action.type) {
-    case REGISTER: {
-      // blank
-      return {
-        ...state,
-      };
-    }
+import {Dispatch} from "redux";
+import {api} from "../../DAL/api";
 
-    default: {
-      return state;
+type InitialStateType = typeof initialState
+
+export const initialState = {
+  isRegistration: false,
+  message: '',
+}
+
+type actionType = successRegistrationType
+
+ export const registerReducer = (state: InitialStateType = initialState, action: actionType): InitialStateType => {
+    switch (action.type) {
+      case "ISSUCCESSREGISTRATION":
+        return {...state, isRegistration: true}
+
+      default: {
+        return state;
+      }
     }
-  }
-};
+  };
+
+export const successRegistrationAC = (isRegistration: boolean) => {
+  return {type: 'ISSUCCESSREGISTRATION', isRegistration } as const
+}
+
+export const registrationTC = (payload : {email: string, password: string}) => (dispatch: Dispatch) => {
+  api.register(payload)
+      .then((res)=>{
+        dispatch(successRegistrationAC(true))
+      })
+}
+
+type successRegistrationType = ReturnType<typeof successRegistrationAC>
