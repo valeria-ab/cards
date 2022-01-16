ae94e2482388f487e0716de25ac0728bd1cf085c
Pavel Kardash <cardash@mail.ru>
2022-01-16 19:40:31 +0300

    modals + auth me + ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React, {useEffect} from 'react';
-import {HashRouter, Navigate} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import React from 'react';
+import {HashRouter} from "react-router-dom";
 import './App.css';
 import Main from './UI/Main';
-import {checkAuthMe} from "./BLL/login/loginThunk";
-import {IAppStore} from "./BLL/store/store";
-
 
 
 const App: React.FC = () => {
-
 
 
     return (
--- src/UI/Main.tsx
+++ src/UI/Main.tsx
@@ -1,16 +1,11 @@
-import React, {useEffect} from "react";
+import React from "react";
 import Header from "./Header";
 import RoutesComponent from "./Routes";
-import {useDispatch, useSelector} from "react-redux";
-import {checkAuthMe} from "../BLL/login/loginThunk";
-import {IAppStore} from "../BLL/store/store";
-import { Navigate } from "react-router-dom";
+import {useDispatch} from "react-redux";
 
 // headers, routes, footers
 const Main: React.FC = () => {
     const dispatch = useDispatch()
-
-
 
 
     return (
