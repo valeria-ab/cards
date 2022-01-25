fd6e805c6f5e1551a50cf0bac272de9583cbed6b
valeria-ab <lera.magnum@gmail.com>
2022-01-25 14:24:51 +0300

    RateYourself UI

---
+++ src/UI/Rate/RateYourself.tsx
@@ -0,0 +1,10 @@
+export const RateYourself = () => {
+    return <div>
+        <div>Rate yourself</div>
+        <div><input type={'radio'}/><span>Did not know</span></div>
+        <div><input type={'radio'}/><span>Forgot</span></div>
+        <div><input type={'radio'}/><span>A lot of thoughts</span></div>
+        <div><input type={'radio'}/><span>Confused</span></div>
+        <div><input type={'radio'}/><span>Knew the answer</span></div>
+    </div>
+}
