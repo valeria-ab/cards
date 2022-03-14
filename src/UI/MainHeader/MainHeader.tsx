82edf06ebf0e541d0197c82250298e4a66ac3261
valeria-ab <lera.magnum@gmail.com>
2022-03-14 22:47:56 +0300

    header refactoring

--- src/UI/MainHeader/MainHeader.tsx
+++ src/UI/MainHeader/MainHeader.tsx
@@ -5,24 +5,28 @@ import LogoutIcon from '../../image/logout.png'
 import {NavLink} from 'react-router-dom';
 import {PACKS_LIST_PATH, PROFILE_PATH} from '../Routes';
 import React from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {setCardsPacksCountFromRangeAC, setSortPacksValueAC, setWithMyIdAC} from '../../BLL/packs/packs-reducer';
 import {logOut} from '../../BLL/login/login-reducer';
 import {changeLayoutAC} from '../../BLL/cards/cards-reducer';
+import {IAppStore} from '../../BLL/store/store';
 
-export  const MainHeader = React.memo( () =>{
+export const MainHeader = React.memo(() => {
     const dispatch = useDispatch()
+    const isInitialized = useSelector<IAppStore, boolean>(state => state.app.isInitialized)
 
     return (
         <div className={s.MainHeader}>
             <div className={s.wrapper}>
                 <h1 className={s.title}>PLAYING CARDS</h1>
-                <div className={s.btnWrap}>
+
+
+                {isInitialized && <div className={s.btnWrap}>
                     <NavLink to={PACKS_LIST_PATH}>
                         <button className={s.btn} onClick={() => {
                             dispatch(setWithMyIdAC(false))
-                            dispatch(setCardsPacksCountFromRangeAC([0,1000]))
-                            dispatch(changeLayoutAC("packs-list"))
+                            dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
+                            dispatch(changeLayoutAC('packs-list'))
                             dispatch(setSortPacksValueAC(null))
                         }}>
                             <img className={s.btnImg} src={PackListIcon} alt="PacksListIcon"/>
@@ -32,25 +36,25 @@ export  const MainHeader = React.memo( () =>{
                     <NavLink to={PROFILE_PATH}>
                         <button className={s.btn} onClick={() => {
                             dispatch(setWithMyIdAC(true))
-                            dispatch(setCardsPacksCountFromRangeAC([0,1000]))
-                            dispatch(changeLayoutAC("profile"))
+                            dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
+                            dispatch(changeLayoutAC('profile'))
                             dispatch(setSortPacksValueAC(null))
                         }}>
                             <img className={s.btnImg} src={ProfileIcon} alt="ProfileIcon"/>
                             <span>Profile</span>
                         </button>
                     </NavLink>
+                    <button className={s.btn}
+                            onClick={() => {
+                                dispatch(logOut())
+                            }}
+                    >
+                        <img className={`${s.btnImg} ${s.btnLogout}`} src={LogoutIcon} alt="ProfileIcon"/>
+                        <span>Logout</span>
+                    </button>
+                </div>}
 
-                        <button className={s.btn}
-                                onClick={() => {
-                                    dispatch(logOut())
-                                }}
-                        >
-                            <img className={`${s.btnImg} ${s.btnLogout}`} src={LogoutIcon} alt="ProfileIcon"/>
-                            <span>Logout</span>
-                        </button>
 
-                </div>
             </div>
         </div>
     );
