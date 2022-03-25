f407a40f6ffab79a9005e1218a4cf80b0d73dd64
valeria-ab <lera.magnum@gmail.com>
2022-03-25 21:30:02 +0300

    header styles were changed

--- src/UI/MainHeader/MainHeader.tsx
+++ src/UI/MainHeader/MainHeader.tsx
@@ -25,58 +25,65 @@ export const MainHeader = React.memo(() => {
     return (
         <div className={s.mainHeader}>
             <div className={s.wrapper}>
-                <h1 className={s.title}>PLAYING CARDS</h1>
+
+                <div className={s.title}>
+                    <h1>PLAYING CARDS</h1>
+                </div>
 
 
-                {isInitialized && <div className={s.btnWrap}>
-                    <NavLink to={PACKS_LIST_PATH}>
-                        <button className={
-                            buttonActive === 'packs-list'
-                                ? `${s.btn} ${s.active}`
-                                : s.btn
+                    <div className={s.btnWrap}>
+                        {isInitialized &&       <>
+                            <NavLink to={PACKS_LIST_PATH}>
+                                <button className={
+                                    buttonActive === 'packs-list'
+                                        ? `${s.btn} ${s.active}`
+                                        : s.btn
+                                }
+                                        onClick={() => {
+                                            dispatch(setWithMyIdAC(false))
+                                            dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
+                                            // dispatch(changeLayoutAC('packs-list'))
+                                            // dispatch(setSortPacksValueAC(null))
+                                            // setButtonActive('packs-list')
+                                        }}>
+                                    <img className={s.btnImg} src={PackListIcon} alt="PacksListIcon"/>
+                                    <span>Packs List</span>
+                                </button>
+                            </NavLink>
+                            <NavLink to={PROFILE_PATH}>
+                                <button className={
+                                    buttonActive === 'profile'
+                                        ? `${s.btn} ${s.active}`
+                                        : s.btn
+                                }
+                                        onClick={() => {
+                                            dispatch(setWithMyIdAC(true))
+                                            dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
+                                            // dispatch(changeLayoutAC('profile'))
+                                            // dispatch(setSortPacksValueAC(null))
+                                            // setButtonActive('profile')
+                                        }}>
+                                    <img className={s.btnImg} src={ProfileIcon} alt="ProfileIcon"/>
+                                    <span>Profile</span>
+                                </button>
+                            </NavLink>
+                            <button className={
+                                buttonActive === 'logout'
+                                    ? `${s.btn} ${s.active}`
+                                    : s.btn
+                            }
+                                    onClick={() => {
+                                        dispatch(logOut())
+                                        setButtonActive('logout')
+                                    }}
+                            >
+                                <img className={`${s.btnImg} ${s.btnLogout}`} src={LogoutIcon} alt="ProfileIcon"/>
+                                <span>Logout</span>
+                            </button>
+                        </>
                         }
-                                onClick={() => {
-                                    dispatch(setWithMyIdAC(false))
-                                    dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
-                                    // dispatch(changeLayoutAC('packs-list'))
-                                    // dispatch(setSortPacksValueAC(null))
-                                    // setButtonActive('packs-list')
-                                }}>
-                            <img className={s.btnImg} src={PackListIcon} alt="PacksListIcon"/>
-                            <span>Packs List</span>
-                        </button>
-                    </NavLink>
-                    <NavLink to={PROFILE_PATH}>
-                        <button className={
-                            buttonActive === 'profile'
-                                ? `${s.btn} ${s.active}`
-                                : s.btn
-                        }
-                                onClick={() => {
-                                    dispatch(setWithMyIdAC(true))
-                                    dispatch(setCardsPacksCountFromRangeAC([0, 1000]))
-                                    // dispatch(changeLayoutAC('profile'))
-                                    // dispatch(setSortPacksValueAC(null))
-                                    // setButtonActive('profile')
-                                }}>
-                            <img className={s.btnImg} src={ProfileIcon} alt="ProfileIcon"/>
-                            <span>Profile</span>
-                        </button>
-                    </NavLink>
-                    <button className={
-                        buttonActive === 'logout'
-                            ? `${s.btn} ${s.active}`
-                            : s.btn
-                    }
-                            onClick={() => {
-                                dispatch(logOut())
-                                setButtonActive('logout')
-                            }}
-                    >
-                        <img className={`${s.btnImg} ${s.btnLogout}`} src={LogoutIcon} alt="ProfileIcon"/>
-                        <span>Logout</span>
-                    </button>
-                </div>}
+                </div>
+
 
 
             </div>
